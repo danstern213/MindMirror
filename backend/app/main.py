5fd4f359dd99feaba3d4baa3ccf2e677b4e1cc77
danstern213 <danstern213@gmail.com>
2025-02-13 21:18:08 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
