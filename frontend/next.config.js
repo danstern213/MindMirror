851779e8fbbcdd420f7f486e684819d12f2e0dc3
danstern213 <danstern213@gmail.com>
2025-02-19 22:04:05 -0500

    Update Next.js configuration files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  trailingSlash: false,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
